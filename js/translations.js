47df6ec1acc618fd12c266169031c47b5f38a1e7
adjoumarwa <104981100+adjoumarwa@users.noreply.github.com>
2026-04-12 05:35:10 +0000

    Update translations.js

--- js/translations.js
+++ js/translations.js
@@ -9,7 +9,7 @@ function addTranslateStyles() {
     const link = document.createElement('link');
     link.id = 'translate-styles-link';
     link.rel = 'stylesheet';
-    link.href = 'js/translate.css';
+    link.href = 'js/translations.css';
     document.head.appendChild(link);
   }
 }
